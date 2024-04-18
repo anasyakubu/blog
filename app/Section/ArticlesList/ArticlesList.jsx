988c9ba3eece1696538558f4e5d2724824b9a3f1
anasyakubu <yakubuanas04@gmail.com>
2024-04-18 15:48:12 +0100

    Articles section

--- app/Section/ArticlesList/ArticlesList.jsx
+++ app/Section/ArticlesList/ArticlesList.jsx
@@ -1,18 +1,61 @@
 import ArticlesListCard from "@/components/shared/ArticlesListCard";
 import React from "react";
+import Image from "next/image";
+import User from "../../assets/user-1.jpg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const ArticlesList = () => {
   return (
     <div className="ArticlesList">
       <div className="p-10">
-        <div className="p-3 space-y-2 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
-          <div className="">
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-2/3 mb-4 md:mb-0">
+            {/* <!-- Content for the 2/3 width section --> */}
             <div className="">
               {/* user image and details */}
-              <div className=""></div>
+              <div className="flex gap-5">
+                <span>/</span>
+                <span>
+                  <Image
+                    className="h-10 w-10 rounded-full"
+                    src={User}
+                    alt="User"
+                  />
+                </span>
+                <span className="text-sm font-light">
+                  {" "}
+                  by{" "}
+                  <small className="font-semibold underline">
+                    <Link href="/">Anas Yakubu</Link>
+                  </small>{" "}
+                  on <b>April 11, 2024</b>
+                </span>
+              </div>
+              <div className="mt-5">
+                <h2
+                  className="text-2xl font-bold text-black hover:underline"
+                  style={{ fontSize: "2rem", lineHeight: "2rem" }}
+                >
+                  <Link href="/">The Ultimate Guide To Email Marketing</Link>
+                </h2>
+                <p className="mt-3 text-sm p-2">Email Marketing</p>
+              </div>
+              <div className="mt-5">
+                <p>
+                  Email marketing is one of the most popular marketing channels
+                  because it’s relatively easy to use, highly effective, and
+                  cheap….
+                </p>
+              </div>
+              <div className="mt-5">
+                <Button>Continue Reading</Button>
+              </div>
             </div>
           </div>
-          <div className="">
+          <div className="w-full md:w-1/3">
+            {/* <!-- Content for the 1/3 width section --> */}
+            <ArticlesListCard />
             <ArticlesListCard />
           </div>
         </div>
--- components/shared/ArticlesListCard.jsx
+++ components/shared/ArticlesListCard.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Image from "next/image";
+import User from "../../app/assets/user-1.jpg";
+import Link from "next/link";
 
 const ArticlesListCard = () => {
-  return <div className="ArticlesListCard">ArticlesListCard</div>;
+  return (
+    <div className="ArticlesListCard">
+      <div className="p-3 m-3 bg-white text-black border border-black rounded-xl shadow-xl">
+        <div className="">
+          <div className="flex gap-5">
+            <span>/</span>
+            <span>
+              <Image className="h-10 w-10 rounded-full" src={User} alt="User" />
+            </span>
+            <span className="text-xl py-2 font-light">
+              <small className="font-semibold ">
+                <Link href="/">Anas Yakubu</Link>
+              </small>{" "}
+            </span>
+          </div>
+          <div className="">
+            <div className="mt-5 py-3">
+              <h2
+                className="text-2xl font-bold text-black hover:underline"
+                style={{ fontSize: "1.5rem", lineHeight: "2.2rem" }}
+              >
+                <Link href="/">
+                  {" "}
+                  How To Optimize Your Website For Local Search in 2024 SEO
+                </Link>
+              </h2>
+              <p className="mt-5 text-sm p-2">Copy Writing Marketing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ArticlesListCard;
