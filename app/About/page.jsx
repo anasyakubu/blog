3d282a3f6a9e3807718f0f46cd7c87b6a1b214e8
anasyakubu <yakubuanas04@gmail.com>
2024-04-19 14:05:46 +0100

    About section

---
+++ app/About/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AboutHeader from "../Section/AboutHeader/AboutHeader";
+import Footer from "../Section/Footer/Footer";
+import Subscribe from "../Section/Subscribe/Subscribe";
+
+const About = () => {
+  return (
+    <div className="About">
+      <div className="inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <AboutHeader />
+        <Subscribe />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/Section/AboutHeader/AboutHeader.jsx
@@ -0,0 +1,47 @@
+import Nav from "@/components/shared/Nav";
+import React from "react";
+import "./AboutHeader.scss";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import BannerOne from "../../assets/Banner-1.svg";
+
+const AboutHeader = () => {
+  return (
+    <div className="AboutHeader">
+      <Nav />
+      <div className="lg:px-6 py-1">
+        <div className="text-center pt-5 p-20">
+          <div className="">
+            <h1
+              className="text-3xl font-extrabold text-black sm:py-5"
+              style={{
+                fontWeight: "600",
+              }}
+            >
+              <span className="text-4xl md:text-[4rem] font-extrabold mt-1 leading-none">
+                We Don’t Just Teach Content Marketing — It’s How We Built Our
+                Company
+              </span>
+            </h1>
+          </div>
+          {/* Image */}
+          <div className="mt-1 flex justify-center text-center">
+            <Image src={BannerOne} alt="Image" />
+          </div>
+          <div className="">
+            <p className="text-lg text-black">
+              Good writing isn’t enough to attract and hold attention. Great
+              writing isn’t enough to attract and hold attention. Anyone who
+              makes a living as a writer today will tell you that your content
+              has to be made up of strategic ingredients. It has to connect with
+              your target audience and inspire action. Luckily, writers can
+              develop those winning skills.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutHeader;
